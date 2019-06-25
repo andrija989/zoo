94c8b3ebb4811b8e1fae219b6dc4fdf3d1c265fa
Andrija Perovic <andrija989@gmail.com>
2019-06-25 19:12:13 +0200

    Zadatak 5

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,12 +1,31 @@
 <template>
-  
+<div>
+    <form @submit.prevent="addAnimal">
+    <div>
+      <label for="type">Animal type</label>
+      <input type="text" id="type" v-model="newAnimal.type" required>
+    </div>
+    <div>
+      <label for="name">Animal name</label>
+      <input type="text" id="name" v-model="newAnimal.name" required>
+    </div>
+    <div>
+      <label for="dateOfBirth">dateOfBirth</label>
+      <input type="text" id="dateOfBirth" v-model="newAnimal.dateOfBirth" required>
+    </div>
+
+    <div>
+      <button type="submit">Add Animal</button>
+    </div>
+  </form>
     <ul>
       <li v-for='(animal,index) in animals' :key="index">
         {{ animal.type }} {{ animal.name }} - {{ animal.dateOfBirth ? animal.dateOfBirth : "Nepoznat" }}
         <button @click="removeAnimal(index)">Remove Animal</button>
+        <button @click="moveToTop(index)">Move to top</button>
       </li>
     </ul>
-  
+</div>
 </template>
 
 <script>
@@ -32,6 +51,21 @@ export default {
   methods: {
     removeAnimal(index) {
       this.animals.splice(index,1)
+    },
+
+    moveToTop(index) {
+      let animalsCopy = this.animals[index]
+      this.animals.splice(index,1)
+      this.animals.unshift(animalsCopy)
+    },
+
+    addAnimal() {
+      this.animals.push({...this.newAnimal})
+      this.newAnimal = [
+        type = "",
+        name = "",
+        dateOfBirth = ""
+      ]
     }
   }
 }
